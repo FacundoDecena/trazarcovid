fb579ad09bcc9154293d1990079858bfbf6f697f
Facundo Decena <facundo.decena@gmail.com>
2020-11-19 16:58:57 -0300

    First commit

---
+++ src/main/java/ar/com/degedev/trazar_covid/TrazarCovidApplication.java
@@ -0,0 +1,13 @@
+package ar.com.degedev.trazar_covid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrazarCovidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrazarCovidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/degedev/trazar_covid/TrazarCovidApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.degedev.trazar_covid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrazarCovidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
