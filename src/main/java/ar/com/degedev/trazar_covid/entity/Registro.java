d913ba7b059d1319c4bbf2b48cd815e52bbf1344
Facundo Decena <facundo.decena@gmail.com>
2020-11-19 18:11:06 -0300

    Entity Registro created

---
+++ src/main/java/ar/com/degedev/trazar_covid/entity/Registro.java
@@ -0,0 +1,30 @@
+package ar.com.degedev.trazar_covid.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
+public class Registro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+
+//    @ManyToOne
+//    @JoinColumn(name = "comercio_id")
+//    private Comercio comercio;
+
+    private LocalDate fecha;
+
+    private LocalTime horaEntrada;
+
+}
